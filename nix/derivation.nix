1e48983e9b3cea4164dc59eab5b8eda9341ef1c9
Radostin Stoyanov <rstoyanov@fedoraproject.org>
2024-10-22 14:01:17 +0100

    test
    Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>

--- nix/derivation.nix
+++ nix/derivation.nix
@@ -32,6 +32,7 @@ with pkgs; stdenv.mkDerivation {
       yajl
     ] ++ lib.optionals enableCriu [ criu ];
   configureFlags = [ "--enable-static" ] ++ lib.optional (!enableSystemd) [ "--disable-systemd" ];
+  runtimeDependencies = lib.optional (enableCriu) [ criu ];
   preFixup = lib.optionalString (stdenv?cc.cc.libgcc) ''
     addAutoPatchelfSearchPath ${stdenv.cc.cc.libgcc}/lib
     addAutoPatchelfSearchPath ${stdenv.cc.cc.libgcc}/lib64
